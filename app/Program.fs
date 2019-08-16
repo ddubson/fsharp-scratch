1b79165ac29e1b25bdc96dfa0d6f861ca9c54402
Dmitriy Dubson <d.dubson@gmail.com>
2019-08-15 21:09:56 -0400

    Add more examples

--- app/Program.fs
+++ app/Program.fs
@@ -1,7 +1,24 @@
 ﻿open System
 open HelloWorldLibrary
 
+// "let" is a keyword for declaring an immutable value
+let x: int = 5
+let y = 10
+
+// Explicit list of items
+let items = [2;3;4;5]
+
+// Push an item to the front of list
+let allItems = 1 :: items
+
+// Explicit type on the input parameter and return type
+let squareExplicit (x: int): int = x * x
+
+// Implicit type inference on input and output types
+let squareImplicit x = x * x
+
 [<EntryPoint>]
 let main argv =
-    printfn "%s" (convertToJson "Hello")
+    printfn "%s" <| convertToJson "Hello"
+    printfn "Square of %d is %d" <| x <| squareImplicit x
     0 // return an integer exit code
