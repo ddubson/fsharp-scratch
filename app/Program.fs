bbd873acf56782d01eff61cfcf02252ee8fbf976
Dmitriy Dubson <d.dubson@gmail.com>
2019-11-04 21:11:41 -0500

    Add option example

--- app/Program.fs
+++ app/Program.fs
@@ -17,8 +17,16 @@ let squareExplicit (x: int): int = x * x
 // Implicit type inference on input and output types
 let squareImplicit x = x * x
 
+let getItemById (id: int): Option<string> =
+    Some("Item1")
+
 [<EntryPoint>]
 let main argv =
     printfn "%s" <| convertToJson "Hello"
     printfn "Square of %d is %d" <| x <| squareImplicit x
+    
+    // Matching an example on an Option monad
+    match (getItemById 1) with
+          | Some item -> printfn "%s!" <| item
+          | None -> printfn "Got nothing"
     0 // return an integer exit code
