31cd08f145804360d7ae53ac09d4115c365784bb
Dmitriy Dubson <d.dubson@gmail.com>
2019-12-15 12:05:49 -0500

    discriminated unions example

---
+++ app/DiscriminatedUnionsExample.fs
@@ -0,0 +1,14 @@
+module app.DiscriminatedUnionsExample
+
+type SuccessfulWithdrawal = { Amount: decimal; Balance: decimal }
+type FailedWithdrawal = { Amount: decimal; Balance: decimal; isOverdraft: bool }
+type MoneyWithdrawalResult =
+    | Success of SuccessfulWithdrawal
+    | InsufficientFunds of FailedWithdrawal
+    | CardExpired of System.DateTime
+    | UndisclosedFailure
+    
+let handleWithdrawal amount =
+    let w = withdrawMoney amount
+    match w with
+    | Success s -> printfn "Successfully withdrew %f" <| s.Amount
